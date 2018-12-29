5f4bddf76c5037b8b472c093e04c820e5db3c9b4
pessonnier <loic.pessonnier@gmail.com>
2018-12-29 01:23:13 +0100

    init

---
+++ step.py
@@ -0,0 +1,54 @@
+import pigpio
+import time
+
+# brochage BMC voir https://fr.pinout.xyz/pinout/pin12_gpio18#
+# correspondent aux sorties 1,2,3,4 du L298N
+outs= [18, 17, 27, 22]
+
+halfstep_seq = [
+  [1,0,0,0],
+  [1,1,0,0],
+  [0,1,0,0],
+  [0,1,1,0],
+  [0,0,1,0],
+  [0,0,1,1],
+  [0,0,0,1],
+  [1,0,0,1]
+]
+
+# les colonnes correspondent aux pins 1,3,4,6 du moteur
+step_seq = [
+  [1,1,0,0],
+  [0,1,1,0],
+  [0,0,1,1],
+  [1,0,0,1]
+]
+
+step_seq2 = [
+  [1,1,0,0],
+  [0,0,1,1],
+  [0,1,1,0],
+  [1,0,0,1]
+]
+
+periode = 2/10000
+
+pi = pigpio.pi()
+
+for pin in outs:
+    pi.set_mode(pin, pigpio.OUTPUT)
+
+def step1(seq = step_seq, periode = periode):
+    for step in seq:
+        for pin, state in zip(outs, step):
+            pi.write(pin, state)
+        time.sleep(periode)
+
+
+def stepn(n, step_fun=step1, seq = step_seq, periode = periode):
+    for i in range(n):
+        step_fun(seq = seq, periode = periode)
+    for pin in outs:
+        pi.write(pin, 0)
+
+stepn(100, seq = step_seq, periode = 2/10000)
